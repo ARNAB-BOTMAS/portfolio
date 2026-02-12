651c277938ac57027fca9767c528419fc89d50c4
Arnab Mondal <92840854+ARNAB-BOTMAS@users.noreply.github.com>
2026-02-12 23:57:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './sass/frame.scss';
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    setTimeout(()=>setLoading(false),3000)
+    setTimeout(()=>setLoading(false),1000)
   }, []);
   return (
     <div className="App">
